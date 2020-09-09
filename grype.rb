660a58634873fe9b514ce5cb8af38aadb9b25341
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-09 10:25:41 -0700

    Brew formula update for grype version v0.1.0-beta.7

--- grype.rb
+++ grype.rb
@@ -2,16 +2,16 @@
 class Grype < Formula
   desc "A vulnerability scanner for container images and filesystems"
   homepage "https://github.com/anchore/grype"
-  version "0.1.0-beta.6"
+  version "0.1.0-beta.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/anchore/grype/releases/download/v0.1.0-beta.6/grype_0.1.0-beta.6_darwin_amd64.tar.gz"
-    sha256 "82ef2c8457e8d806caf275c7bfc9e992f497c1bcb5388b8d8dc1094a98cfc187"
+    url "https://github.com/anchore/grype/releases/download/v0.1.0-beta.7/grype_0.1.0-beta.7_darwin_amd64.tar.gz"
+    sha256 "ccfca5d5f461196974198507ff12f585eb1c92cbf75f834ee0e01b26575a4133"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/grype/releases/download/v0.1.0-beta.6/grype_0.1.0-beta.6_linux_amd64.tar.gz"
-      sha256 "70d12962c5c7b8fd992ce31878eaa0a71a9b0bf18fd1235f9fbabe8b23a2e5b7"
+      url "https://github.com/anchore/grype/releases/download/v0.1.0-beta.7/grype_0.1.0-beta.7_linux_amd64.tar.gz"
+      sha256 "e51199d1a90c7d334046d65f3431e5b0cc66a381e4e2cf3a7119841e3946a560"
     end
   end
 
