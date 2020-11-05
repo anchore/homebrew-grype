83ef69ecde08d5c0bde0081a6f5cafaec164acaf
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-05 13:00:27 -0800

    Brew formula update for grype version v0.3.0

--- grype.rb
+++ grype.rb
@@ -2,20 +2,21 @@
 class Grype < Formula
   desc "A vulnerability scanner for container images and filesystems"
   homepage "https://github.com/anchore/grype"
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/anchore/grype/releases/download/v0.2.0/grype_0.2.0_darwin_amd64.tar.gz"
-    sha256 "c99985ed5b78c17401e132b9beececa11bcd46b82a6967c94dfe47a0b3ad918d"
+    url "https://github.com/anchore/grype/releases/download/v0.3.0/grype_0.3.0_darwin_amd64.tar.gz"
+    sha256 "db8b36dd93c668022eb729d6fb37c9f972a5c9c0e9d3fd5394a18d6c04c83d04"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/grype/releases/download/v0.2.0/grype_0.2.0_linux_amd64.tar.gz"
-      sha256 "52af0392c7ee6afd03fc29d832c3d1809b8633de021f38ddc0dbc92a1638225b"
+      url "https://github.com/anchore/grype/releases/download/v0.3.0/grype_0.3.0_linux_amd64.tar.gz"
+      sha256 "dcb09632869404af328b8b54fa5eacacecd9ffae778f40d037d200c6142d3c70"
     end
   end
 
   def install
+    bin.install "grype"
     bin.install "grype"
   end
 end
